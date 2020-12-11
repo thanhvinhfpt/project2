6119607078da4a75e1c6c679c786c4ba0a9ab6ae
Sainteric93hd <58420787+Sainteric93hd@users.noreply.github.com>
2020-12-11 08:51:44 +0700

    last update

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,19 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Cviebrock\EloquentSluggable\Sluggable;
+
 
 class Tag extends Model
 {
     use HasFactory;
-    use Sluggable;
-    public function sluggable(){
-        return ['slug' => [
-            'source' => 'title'
-        ]];
-        // TODO: Implement sluggable() method.
-    }
-
     public function post(){
         return $this->hasMany('App\Models\Post');
     }
